518a877ec90182d91e1d0239e5042a92eb9b78b7
Gregory V. Bilello <gregory.bilello@gmail.com>
2016-12-12 14:44:07 -0500

    Adding PlayState and Map classes.

---
+++ states/play_state.rb
@@ -0,0 +1,33 @@
+class PlayState < GameState
+
+	def initialize
+		@map = Map.new
+		@player = Player.new(@map)
+		@camera = Camera.new(@player)
+	end
+
+	def update
+		@camera.update
+	end
+
+	def draw
+		cam_x = @camera.x
+		cam_y = @camera.y
+		off_x = $window.width / 2 - cam_x
+		off_y = $window.height / 2 - cam_y
+		$window.translate(off_x, off_y) do
+			zoom = @camera.zoom
+			$window.scale(zoom, zoom, cam_x, cam_y) do
+				@map.draw(camera)
+				@player.draw
+			end
+		end
+	end
+
+	def button_down(id)
+		if id == Gosu::KbEscape
+			GameState.switch(MenuState.instance)
+		end
+	end
+
+end
