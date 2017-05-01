887943ce545ec25c258bb2e9985facaab32e4798
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-05-01 14:04:09 -0400

    Added note in main.rb about changing to a tile-based game-object system to draw maps and items - functions to be implemented based on notes.

--- main.rb
+++ main.rb
@@ -37,6 +37,8 @@ class MazeWindow < Gosu::Window
   def update
   end
 
+  # need to creat functions to create game objects (floor tiles, wall tiles, door tiles, etc.) and give those objects a state declaring whether or not the player character can interact with those items.  Ex: walls and other items will be impassable.  Instead of the using (x, y) position variables to determine if the player can continue moving in a certain direction, a function can be created to compare the tile the character is currently on with the tile the player is attempting to move to, prohibiting movement if necessary.  This method can also allow for other interactable tiles (ex. doors, stairs, items, etc.) to have a state telling the game what to do when the player character interacts with them.
+
   # code to draw and display maze with Gosu below
 
   # def draw
